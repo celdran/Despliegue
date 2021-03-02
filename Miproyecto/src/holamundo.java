a7eb16c0afdc74ebc96d81dc4edad7c2152c167e
Celdrán <64540000+celdran@users.noreply.github.com>
2021-03-02 18:43:54 +0100

    Add files via upload

---
+++ Miproyecto/src/holamundo.java
@@ -0,0 +1,12 @@
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola mundo");
+		
+		
+		System.out.print("Hola mundo2");
+	}
+
+}
---
+++ Miproyecto/src/prueba1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class prueba1 {
+	public static void main( String[] args )
+    {
+		System.out.print("Hola mundo");
+		
+		
+        int n1, n2, suma;
+
+        Scanner teclado = new Scanner( System.in );
+
+        System.out.print( "Introduzca primer número: " );
+        n1 = teclado.nextInt();
+
+        System.out.print( "Introduzca segundoo número: " );
+        n2 = teclado.nextInt();
+
+        suma = n1 + n2;
+
+        System.out.println( "La suma de " + n1 + " más " + n2 + " es " + suma + "." );
+    }
+}
+	
